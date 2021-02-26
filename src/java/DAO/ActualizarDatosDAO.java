685429cb89b3131ebaf2909e9386558f9148c2eb
Daniel Hernàndez Lozano <danteher97@outlook.es>
2021-02-26 16:56:52 +0000

    Updated ActualizarDatosDAO.java

--- src/java/DAO/ActualizarDatosDAO.java
+++ src/java/DAO/ActualizarDatosDAO.java
@@ -21,6 +21,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
@@ -53,7 +54,7 @@ public class ActualizarDatosDAO {
                 estudiante.setNombres(consulta.getString("nombres"));
                 estudiante.setFecha_nacimiento(date);  //consulta.getDate("fecha_nacimiento"));
                 estudiante.setGenero(consulta.getString("genero"));
-                estudiante.setFoto((consulta.getString("foto") == null)? "assets/img/user-default.png":consulta.getString("foto"));
+                estudiante.setFoto((consulta.getString("foto") == null || consulta.getString("foto").equals(""))? "assets/img/user-default.png":consulta.getString("foto"));
                 estudiante.setDireccion(consulta.getString("direccion"));
                 estudiante.setFoto_domicilio(consulta.getString("foto_domicilio"));
                 estudiante.setCarnet_discapacidad(consulta.getString("carnet_discapacidad"));
@@ -63,8 +64,8 @@ public class ActualizarDatosDAO {
                 estudiante.setNumero_hermanos(consulta.getInt("numero_hermanos"));
                 estudiante.setLugar_ocupa(consulta.getInt("lugar_ocupa"));
                 estudiante.setNacionalidad(consulta.getString("nacionalidad"));
-                estudiante.setNacionalidad(consulta.getString("correo"));
-                estudiante.setNacionalidad(consulta.getString("celular"));
+                estudiante.setCorreo(consulta.getString("correo_personal"));
+                estudiante.setCelular(consulta.getString("celular"));
             }
 
         } catch (SQLException ex) {
@@ -213,19 +214,104 @@ public class ActualizarDatosDAO {
     }
     
     public boolean actualizarEstudiante(JSONArray datos) throws ParseException, SQLException {
-        /*try {
+        try {
             connecPostgresql = new ConexionPostgreSQL();
             ResultSet consulta;
-            //connecPostgresql.callableStatement = connecPostgresql.connection.prepareCall("{call mostrar_estudiante_convivencia_filtro_persona(?)}");
-            //connecPostgresql.callableStatement.setInt(1, id);
-            //consulta = connecPostgresql.callableStatement.executeQuery();
-            return true;
-
+            
+            //Actualizar Alumnos
+            connecPostgresql.callableStatement = connecPostgresql.connection.prepareCall("{call modificar_estudiante(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}");
+            connecPostgresql.callableStatement.setInt(1, datos.getJSONObject(44).getInt("value")); //id
+            connecPostgresql.callableStatement.setInt(2, datos.getJSONObject(9).getInt("value")); //id_curso
+            connecPostgresql.callableStatement.setString(3, datos.getJSONObject(1).getString("value")); //identificacion
+            connecPostgresql.callableStatement.setString(4, datos.getJSONObject(0).getString("value")); //tipo identificacion
+            connecPostgresql.callableStatement.setString(5, datos.getJSONObject(6).getString("value")); //apellidos
+            connecPostgresql.callableStatement.setString(6, datos.getJSONObject(5).getString("value")); //nombres
+            String fecha = datos.getJSONObject(4).getString("value");
+            fecha = fecha.replace("-", "/");
+            Date fecha_nacimiento = new Date(fecha);
+            connecPostgresql.callableStatement.setDate(7, new java.sql.Date(fecha_nacimiento.getTime())); //fecha
+            connecPostgresql.callableStatement.setString(8, datos.getJSONObject(3).getString("value"));
+            connecPostgresql.callableStatement.setString(9, ""); //foto 50
+            connecPostgresql.callableStatement.setString(10, datos.getJSONObject(10).getString("value"));
+            connecPostgresql.callableStatement.setString(11, ""); // foto domicilio 51
+            connecPostgresql.callableStatement.setString(12, datos.getJSONObject(15).getString("value"));
+            connecPostgresql.callableStatement.setString(13, datos.getJSONObject(14).getString("value"));
+            connecPostgresql.callableStatement.setString(14, datos.getJSONObject(13).getString("value"));
+            connecPostgresql.callableStatement.setString(15, datos.getJSONObject(16).getString("value"));
+            connecPostgresql.callableStatement.setInt(16, datos.getJSONObject(11).getInt("value")); /// Numero Hermanos
+            connecPostgresql.callableStatement.setInt(17, datos.getJSONObject(12).getInt("value")); // Lugar Ocupa
+            connecPostgresql.callableStatement.setString(18, datos.getJSONObject(2).getString("value"));
+            connecPostgresql.callableStatement.setString(19, datos.getJSONObject(7).getString("value"));
+            connecPostgresql.callableStatement.setString(20, datos.getJSONObject(8).getString("value"));
+            consulta = connecPostgresql.callableStatement.executeQuery();
+            
+            //Actualizar Referencia
+            int id_referencia = 0;
+            if(datos.getJSONObject(49).getInt("value") == 0){
+                //No existe se registra
+                connecPostgresql.callableStatement = connecPostgresql.connection.prepareCall("{call registrar_referencia(?,?,?,?,?,?)}");
+                connecPostgresql.callableStatement.setString(1, datos.getJSONObject(40).getString("value")); //apellidos
+                connecPostgresql.callableStatement.setString(2, datos.getJSONObject(39).getString("value")); //nombres
+                connecPostgresql.callableStatement.setString(3, datos.getJSONObject(43).getString("value")); //celular
+                connecPostgresql.callableStatement.setString(4, datos.getJSONObject(42).getString("value")); //telefono
+                connecPostgresql.callableStatement.setString(5, datos.getJSONObject(38).getString("value")); //identificacion
+                connecPostgresql.callableStatement.setString(6, datos.getJSONObject(37).getString("value")); //tipo_identificacion
+                consulta = connecPostgresql.callableStatement.executeQuery();    
+                
+                //Consultar ID
+                connecPostgresql.callableStatement = connecPostgresql.connection.prepareCall("{call obtener_idreferencia_filtro_todo(?,?,?,?,?,?)}");
+                connecPostgresql.callableStatement.setString(1, datos.getJSONObject(40).getString("value")); //apellidos
+                connecPostgresql.callableStatement.setString(2, datos.getJSONObject(39).getString("value")); //nombres
+                connecPostgresql.callableStatement.setString(3, datos.getJSONObject(43).getString("value")); //celular
+                connecPostgresql.callableStatement.setString(4, datos.getJSONObject(42).getString("value")); //telefono
+                connecPostgresql.callableStatement.setString(5, datos.getJSONObject(38).getString("value")); //identificacion
+                connecPostgresql.callableStatement.setString(6, datos.getJSONObject(37).getString("value")); //tipo_identificacion
+                consulta = connecPostgresql.callableStatement.executeQuery();       
+                while (consulta.next()) {
+                    id_referencia = consulta.getInt(1);
+                }
+                
+            }else{
+                //Ya existe se actualiza
+                connecPostgresql.callableStatement = connecPostgresql.connection.prepareCall("{call modificar_referencia(?,?,?,?,?,?,?)}");
+                connecPostgresql.callableStatement.setInt(1, datos.getJSONObject(49).getInt("value")); //ID
+                connecPostgresql.callableStatement.setString(2, datos.getJSONObject(40).getString("value")); //apellidos
+                connecPostgresql.callableStatement.setString(3, datos.getJSONObject(39).getString("value")); //nombres
+                connecPostgresql.callableStatement.setString(4, datos.getJSONObject(43).getString("value")); //celular
+                connecPostgresql.callableStatement.setString(5, datos.getJSONObject(42).getString("value")); //telefono
+                connecPostgresql.callableStatement.setString(6, datos.getJSONObject(38).getString("value")); //identificacion
+                connecPostgresql.callableStatement.setString(7, datos.getJSONObject(37).getString("value")); //tipo_identificacion
+                id_referencia = datos.getJSONObject(49).getInt("value");
+                consulta = connecPostgresql.callableStatement.executeQuery();
+            }
+            
+            //Insertar Relacion Estudiante_Referencia
+            if(id_referencia != 0){
+                int id_estreferencia = datos.getJSONObject(50).getInt("value");
+                if(id_estreferencia == 0){
+                    //No existe se registra
+                    connecPostgresql.callableStatement = connecPostgresql.connection.prepareCall("{call registrar_estudiante_referencia(?,?,?)}");
+                    connecPostgresql.callableStatement.setInt(1, datos.getJSONObject(44).getInt("value")); //id_estudiante
+                    connecPostgresql.callableStatement.setInt(2, id_referencia); //id_referencia
+                    connecPostgresql.callableStatement.setString(3, datos.getJSONObject(41).getString("value")); //parentesco
+                    consulta = connecPostgresql.callableStatement.executeQuery();
+                }else{
+                    //Ya existe se actualiza
+                    connecPostgresql.callableStatement = connecPostgresql.connection.prepareCall("{call modificar_estudiante_referencia(?,?,?,?)}");
+                    connecPostgresql.callableStatement.setInt(1, id_estreferencia); //id_estdreferencia
+                    connecPostgresql.callableStatement.setInt(2, datos.getJSONObject(44).getInt("value")); //id_estudiante
+                    connecPostgresql.callableStatement.setInt(3, id_referencia); //id_referencia
+                    connecPostgresql.callableStatement.setString(4, datos.getJSONObject(41).getString("value")); //parentesco
+                    consulta = connecPostgresql.callableStatement.executeQuery();
+                }
+            }
+            
+            return true;    
         } catch (SQLException ex) {
             connecPostgresql.getConnection().close();
             System.out.println(ex.getMessage());
         }
-        connecPostgresql.getConnection().close();*/
+        connecPostgresql.getConnection().close();
         return false;
     }
 }
